7c924d52ead17bd59be62111bea3dee0a0b66170
huseinmirahmatov <165923927+huseinmirahmatov@users.noreply.github.com>
2024-04-04 08:35:59 +0500

    Playlist.cs

--- Domain exp1/Enum/Playlist.cs
+++ Domain exp1/Enum/Playlist.cs
@@ -2,6 +2,7 @@
 {
     public class Playlist
     {
+        public int Id { get; set; }
         public string Title { get; }
         public string Track { get; }
         public User User { get; }
